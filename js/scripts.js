dd63fa03b4aea21e48e968ca1a601c12d679d282
Ohene Gyan <Janmillz@yahoo.com>
2017-09-30 15:56:13 +0100

    all files modified

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+      $(".role").click(function(event) {
+            event.preventDefault();
+            var randomNum = math.floow()
+            $(".role").click(function)
